23a5a1341758705bebd4b28cb55f679c0fb20454
ayushimakhija <ayushi9yn.19@gmail>
2021-03-09 23:24:57 +0530

    adding sqaure root function and its testcases

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,136 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Calculator {
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
+
+    public Calculator() {
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+        double number1=0, number2;
+        do {
+            System.out.println("Scientific Calculator, Choose to perform operation");
+            System.out.print("Press 1 to Square root function\nPress 2 to Factorial function\nPress 3 to Natural logrithm\nPress 4 to power function\n" +
+                    "Press any  key to exit\nEnter your choice: ");
+            int choice;
+            try {
+                choice = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                return ;
+            }
+            try {
+                if (choice == 1 || choice == 2 || choice == 3 || choice == 4) {
+                    System.out.print("Enter the first number : ");
+                    number1 = scanner.nextDouble();
+                }
+                if (choice == 4) {
+                    System.out.print("Enter the second number : ");
+                    number2 = scanner.nextDouble();
+                }
+            } catch (InputMismatchException e) {
+                logger.error("Invalid input, Entered input is not a number");
+                return;
+            }
+            switch (choice) {
+                case 1:
+                    //Square root
+                    System.out.println("Square root  is : " + calculator.Square_root(number1));
+                    break;
+                /*case 2:
+                    //Factorial function
+                    System.out.println("Factorial is : " + calculator.Fact(number1));
+                    break;
+                case 3:
+                    // Natural Logrithm
+                    System.out.println("Natural logrithm is : " + calculator.Natural_log(number1));
+                    break;
+                case 4:
+                    //Power Function
+                    System.out.println("power is : " + calculator.Power(number1, number2));
+                    break;*/
+                default:
+                    System.out.println("Exiting....");
+                    return;
+            }
+        } while (true);
+    }
+
+    public double Square_root(double number1) {
+        double result = 0;
+        try {
+            logger.info("Square root of number " + number1);
+            if (number1 < 0 || number1 == Double.NaN) {
+                result = Double.NaN;
+                throw new ArithmeticException("Case of NaN 0.0/0.0");
+            } else if (number1 == Double.POSITIVE_INFINITY) {
+                result = Double.POSITIVE_INFINITY;
+                throw new ArithmeticException("Case of Positive Infinity 1.0/0.0");
+            } else {
+                result = Math.sqrt(number1);
+            }
+        } catch (ArithmeticException error) {
+            logger.error(" sqrt of a number cannot be find" + error.getLocalizedMessage());
+        } finally {
+            logger.info("Result of Square root  is " + result);
+        }
+
+        return result;
+
+}
+
+
+  /*  public double Fact(double number1)
+            {
+            }
+
+public double Natural_log(double number1) {
+        double result = 0;
+        try {
+        logger.info("NAtural logrithm of numbers " + number1 + " and " + number2);
+        if (number1 < 0 || number1 == Double.NaN) {
+        result = Double.NaN;
+        throw new ArithmeticException("Case of NaN 0.0/0.0");
+        } else if (number1 == Double.POSITIVE_INFINITY) {
+        result = Double.POSITIVE_INFINITY;
+        throw new ArithmeticException("Case of Positive Infinity 1.0/0.0");
+        } else if (number1 ==) {
+        result = Double.NEGATIVE_INFINITY;
+        throw new ArithmeticException("Case of Negative Infinity -1.0/0.0");
+        } else {
+        result = Math.log(number1);
+        }
+        } catch (ArithmeticException error) {
+        logger.error(" Natural logrithm of a number cannot be find" + error.getLocalizedMessage());
+        } finally {
+        logger.info("Result of Natural log is " + result);
+        }
+        return result;
+        }
+
+public double Power(double number1, double number2) {
+        double result = 0;
+        try {
+        logger.info("power of numbers " + number1 + " and " + number2);
+        if ( number2 == 0) {
+        result = 1.0;
+        } else if (number2 == 1.0) {
+        result = number1;
+        } else if ( number2 == Double.NaN) {
+        result = Double.NaN;
+        throw new ArithmeticException("Case of Negative Infinity 0.0/0.0");
+        } else {
+        result = Math.pow(number1,number2);
+        }
+        } catch (ArithmeticException error) {
+        logger.error("powers of a number cannot be found " + error.getLocalizedMessage());
+        } finally {
+        logger.info("Result of power is " + result);
+        }
+        return result;
+        }*/
+        }
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,60 @@
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CalculatorTest {
+    private static final double DELTA = 1e-15;
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void SquarerootTruePositive(){
+        assertEquals("Square root for True Positive", 9, calculator.Square_root(81), DELTA);
+        assertEquals("Square root for True Positive", 4.0, calculator.Square_root(16.0), DELTA);
+    }
+
+    @Test
+    public void SquarerootFalsePositive(){
+        assertNotEquals("Square root for False Positive", 8, calculator.Square_root(81), DELTA);
+        assertNotEquals("Square root for False Positive", 7.0, calculator.Square_root(12.0), DELTA);
+    }
+
+    /*@Test
+    public void FactorialTruePositive(){
+        assertEquals("Factorial for True Positive",6, calculator.Fact(3),DELTA);
+        assertEquals("Factorial for True Positive",120, calculator.Fact(5), DELTA);
+
+    }
+
+
+    @Test
+    public void FactorialFalsePositive(){
+        assertNotEquals("Factorial for False Positive",7,calculator.Fact(3),DELTA);
+        assertNotEquals("Factorial for False Positive",120,calculator.Fact(7),DELTA);
+    }
+
+
+    @Test
+    public void NaturallogrithmTruePositive(){
+        assertEquals("Natural logrithm for True Positive",
+                assertEquals("Natural logrithm for True Positive",
+    }
+
+    @Test
+    public void NaturallogrithmFalsePositive(){
+        assertNotEquals("Natural logrithm for False Positive",
+                assertNotEquals("Natural logrithm for False Positive",
+    }
+
+    @Test
+    public void PowerTruePositive(){
+        assertEquals("Power for True Positive", 8, calculator.Power(2,3), DELTA);
+        assertEquals("Power for True Positive", 27.0 , calculator.power(3,3),DELTA);
+    }
+
+    @Test
+    public void PowerTrueNegative(){
+        assertNotEquals("Power for True Negative",5, calculator.Power(2,3), DELTA);
+        assertNotEquals("Power for True Negative",35.0,calculator.Power(4,3), DELTA);
+    }*/
+    }
+
